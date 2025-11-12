8ef23f11c0395797d4720bf88026dfa58fd34fe9
s32121 <s32121@pjwstk.edu.pl>
2025-11-12 19:48:14 +0100

    Add User.java and Merchant.java

---
+++ src/main/java/User.java
@@ -0,0 +1,39 @@
+abstract class User {
+    private String fullName;
+    private String email;
+    private String mobileNumber;
+    private String shippingAddress;
+    private String login;
+    private String password;
+
+    private User(
+            String fullName,
+            String email,
+            String mobileNumber,
+            String shippingAddress,
+            String login,
+            String password
+            ){
+        this.email=email;
+        this.fullName=fullName;
+        this.mobileNumber=mobileNumber;
+        this.shippingAddress=shippingAddress;
+        this.login=login;
+        this.password=password;
+    }
+    public boolean updateProfile(
+            String fullName,
+            String email,
+            String mobileNumber,
+            String shippingAddress,
+            String login,
+            String password) {
+        this.email=email;
+        this.fullName=fullName;
+        this.mobileNumber=mobileNumber;
+        this.shippingAddress=shippingAddress;
+        this.login=login;
+        this.password=password;
+        return true;
+    }
+}
