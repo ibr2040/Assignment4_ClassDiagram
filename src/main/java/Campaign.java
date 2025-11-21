3cabd3336a2f2a5db980d4c641cfb970184e3153
MuslleMummy <glebanator2000pro@gmail.com>
2025-11-21 01:36:20 +0100

    Finalising my part

--- src/main/java/Campaign.java
+++ src/main/java/Campaign.java
@@ -1,19 +1,56 @@
-public class Campaign {
+import java.io.*;
+import java.util.*;
+
+
+public class Campaign implements Serializable {
+    private static final List<Campaign> extent = new ArrayList<>();
+    private static final String EXT_FILE = "campaign_extent.ser";
+
+
     private String campaignName;
     private int budget;
     private String pricePerInteraction;
 
-    Campaign(String campaignName, int budget) {
-        if(!campaignName.equals("")) {
+
+    public Campaign(String campaignName, int budget) {
+        if (!campaignName.equals("")) {
             this.campaignName = campaignName;
-        }else{throw new InvalidFormatException("Invalid campaign name");}
-        if(budget > 0) {
+        } else {
+            throw new InvalidFormatException("Invalid campaign name");
+        }
+        if (budget > 0) {
             this.budget = budget;
-        }else{throw new InvalidValueException("Invalid budget");}
+        } else {
+            throw new InvalidValueException("Invalid budget");
+        }
+
+
+        extent.add(this);
     }
 
-    public int getCampaignFee(){
-        System.out.println(campaignName+" fee calculation in progress");
+
+    public int getCampaignFee() {
+        System.out.println(campaignName + " fee calculation in progress");
         return 0;
+    }
+
+
+    public static List<Campaign> getExtent() { return extent; }
+
+
+    public static void saveExtent() throws Exception {
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(EXT_FILE));
+        oos.writeObject(extent);
+        oos.close();
+    }
+
+
+    public static void loadExtent() throws Exception {
+        File f = new File(EXT_FILE);
+        if (!f.exists()) return;
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(EXT_FILE));
+        List<Campaign> list = (List<Campaign>) ois.readObject();
+        extent.clear(); extent.addAll(list);
+        ois.close();
     }
 }
--- src/main/java/DurationDate.java
+++ src/main/java/DurationDate.java
@@ -1,27 +1,63 @@
-public class DurationDate {
-private Date dateOfEstablishment;
-private Date dateOfExpiration;
-public int TimeLeft(){
-    return dateOfExpiration.getDays() - dateOfEstablishment.getDays();
-}
-    public DurationDate(Date dateOfEstablishment, Date dateOfExpiration){
+import java.io.*;
+import java.util.*;
+
+
+public class DurationDate implements Serializable {
+    private static final List<DurationDate> extent = new ArrayList<>();
+    private static final String EXT_FILE = "duration_extent.ser";
+
+
+    private Date dateOfEstablishment;
+    private Date dateOfExpiration;
+
+
+    public DurationDate(Date dateOfEstablishment, Date dateOfExpiration) {
         this.dateOfEstablishment = dateOfEstablishment;
         this.dateOfExpiration = dateOfExpiration;
-        if(!(TimeLeft()>0)){throw new InvalidValueException("The duration exceeds the maximum number of days");}
+        if (!(TimeLeft() > 0)) {
+            throw new InvalidValueException("The duration exceeds the maximum number of days");
+        }
+        extent.add(this);
     }
-    public static class Date {
+
+
+    public int TimeLeft() {
+        return dateOfExpiration.getDays() - dateOfEstablishment.getDays();
+    }
+
+
+    public static List<DurationDate> getExtent() {
+        return extent;
+    }
+
+
+    public static void saveExtent() throws Exception {
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(EXT_FILE));
+        oos.writeObject(extent);
+        oos.close();
+    }
+
+
+    public static void loadExtent() throws Exception {
+        File f = new File(EXT_FILE);
+        if (!f.exists()) return;
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(EXT_FILE));
+        List<DurationDate> list = (List<DurationDate>) ois.readObject();
+        extent.clear();
+        extent.addAll(list);
+        ois.close();
+    }
+
+
+    public static class Date implements Serializable {
         private int year;
         private int month;
         private int day;
         private int hour;
 
-        Date(int year, int month, int day, int hour) {
 
-            // корректная проверка
-            if (month < 1 || month > 12 ||
-                    day < 1 || day > 31 ||
-                    hour < 0 || hour > 23)
-            {
+        Date(int year, int month, int day, int hour) {
+            if (month < 1 || month > 12 || day < 1 || day > 31 || hour < 0 || hour > 23) {
                 throw new InvalidFormatException("Invalid date format");
             }
             this.year = year;
@@ -29,19 +65,20 @@ public int TimeLeft(){
             this.day = day;
             this.hour = hour;
         }
+
+
         private static final int[] daysInMonth = {
-                31, 28, 31, 30, 31, 30,
-                31, 31, 30, 31, 30, 31
+                31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
         };
 
+
         private boolean isLeapYear() {
-            return (year % 400 == 0) ||
-                    (year % 4 == 0 && year % 100 != 0);
+            return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
         }
+
 
         public int getDays() {
             int days = day;
-
             for (int i = 0; i < month - 1; i++) {
                 days += daysInMonth[i];
             }
@@ -51,4 +88,4 @@ public int TimeLeft(){
             return days;
         }
     }
-    }
+}
